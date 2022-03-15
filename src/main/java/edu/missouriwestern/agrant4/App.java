05a74ab22190919e73ec7ad375627462e74d37ad
abg5043 <aaron.b.grant@gmail.com>
2022-03-14 20:59:59 -0500

    Initial commit

---
+++ src/main/java/edu/missouriwestern/agrant4/App.java
@@ -0,0 +1,13 @@
+package edu.missouriwestern.agrant4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
